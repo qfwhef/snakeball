1ff1f709fbb328e2208b14daafcab84491099eac
qfwhef <843341710@qq.com>
2025-04-07 22:22:48 +0800

    second commit

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: './',
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        process: require.resolve('process/browser'),
+      },
+    },
+    plugins: [
+      require('webpack').ProvidePlugin({
+        process: 'process/browser',
+      }),
+    ],
+  },
 })
